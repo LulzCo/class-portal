96344ba7f458c9f1831964b08ddbbf6a0e15101c
LulzCo <ysw991106@gmail.com>
2023-03-31 18:11:55 +0900

    디비 연결 오류에 대한 장애 복구

--- userDao/src/main/java/kr/ac/jejunu/user/UserDao.java
+++ userDao/src/main/java/kr/ac/jejunu/user/UserDao.java
@@ -13,45 +13,80 @@ public class UserDao {
 
     public User findById(Long id) throws SQLException {
 
-        Connection connection = dataSource.getConnection();
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
+        User user;
 
-        PreparedStatement preparedStatement = connection.prepareStatement(
-                "select id, name, password from userinfo where id = ?");
-        preparedStatement.setLong(1, id);
-        ResultSet resultSet = preparedStatement.executeQuery();
+        try {
+            connection = dataSource.getConnection();
 
-        resultSet.next();
-        User user = new User();
-        user.setId(resultSet.getLong("id"));
-        user.setName(resultSet.getString("name"));
-        user.setPassword(resultSet.getString("password"));
+            preparedStatement = connection.prepareStatement(
+                    "select id, name, password from userinfo where id = ?");
+            preparedStatement.setLong(1, id);
+            resultSet = preparedStatement.executeQuery();
 
-        resultSet.close();
-        preparedStatement.close();
-        connection.close();
-
+            resultSet.next();
+            user = new User();
+            user.setId(resultSet.getLong("id"));
+            user.setName(resultSet.getString("name"));
+            user.setPassword(resultSet.getString("password"));
+        } finally {
+            try {
+                resultSet.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            try {
+                preparedStatement.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
         return user;
     }
 
     public void insert(User user) throws ClassNotFoundException, SQLException {
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        ResultSet resultSet = null;
 
-        Connection connection = dataSource.getConnection();
+        try {
+            connection = dataSource.getConnection();
 
-        PreparedStatement preparedStatement = connection.prepareStatement(
-                "insert into userinfo (name, password) values (?, ?)"
-                , Statement.RETURN_GENERATED_KEYS
-        );
-        preparedStatement.setString(1, user.getName());
-        preparedStatement.setString(2, user.getPassword());
+            preparedStatement = connection.prepareStatement(
+                    "insert into userinfo (name, password) values (?, ?)"
+                    , Statement.RETURN_GENERATED_KEYS
+            );
+            preparedStatement.setString(1, user.getName());
+            preparedStatement.setString(2, user.getPassword());
 
-        preparedStatement.executeUpdate();
-        ResultSet resultSet = preparedStatement.getGeneratedKeys();
-        resultSet.next();
-        user.setId(resultSet.getLong(1));
-
-        resultSet.close();
-        preparedStatement.close();
-        connection.close();
+            preparedStatement.executeUpdate();
+            resultSet = preparedStatement.getGeneratedKeys();
+            resultSet.next();
+            user.setId(resultSet.getLong(1));
+        } finally {
+            try {
+                resultSet.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            try {
+                preparedStatement.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
     }
 
 //    public Connection getConnection() throws ClassNotFoundException, SQLException {
