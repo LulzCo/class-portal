147aa045cb61ad72d03af40b51afb52a9aaee3a9
LulzCo <ysw991106@gmail.com>
2023-04-07 17:54:33 +0900

    리팩터링

--- userDao/src/main/java/kr/ac/jejunu/user/UserDao.java
+++ userDao/src/main/java/kr/ac/jejunu/user/UserDao.java
@@ -12,7 +12,6 @@ public class UserDao {
     }
 
     public User findById(Long id) throws SQLException {
-
         Connection connection = null;
         PreparedStatement preparedStatement = null;
         ResultSet resultSet = null;
@@ -20,7 +19,6 @@ public class UserDao {
 
         try {
             connection = dataSource.getConnection();
-
             preparedStatement = connection.prepareStatement(
                     "select id, name, password from userinfo where id = ?");
             preparedStatement.setLong(1, id);
@@ -58,14 +56,12 @@ public class UserDao {
 
         try {
             connection = dataSource.getConnection();
-
             preparedStatement = connection.prepareStatement(
                     "insert into userinfo (name, password) values (?, ?)"
                     , Statement.RETURN_GENERATED_KEYS
             );
             preparedStatement.setString(1, user.getName());
             preparedStatement.setString(2, user.getPassword());
-
             preparedStatement.executeUpdate();
             resultSet = preparedStatement.getGeneratedKeys();
             resultSet.next();
@@ -90,44 +86,26 @@ public class UserDao {
     }
 
     public void update(User user) throws SQLException {
-        Connection connection = null;
-        PreparedStatement preparedStatement = null;
-
-        try {
-            connection = dataSource.getConnection();
-
-            preparedStatement = connection.prepareStatement(
-                    "update userinfo set name = ?, password = ? where id = ?");
-            preparedStatement.setString(1, user.getName());
-            preparedStatement.setString(2, user.getPassword());
-            preparedStatement.setLong(3, user.getId());
-
-            preparedStatement.executeUpdate();
-        } finally {
-            try {
-                preparedStatement.close();
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-            try {
-                connection.close();
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-        }
+        StatementStrategy statementStrategy = new UpdateStatementStrategy(user);
+        jdbcContextForUpdate(statementStrategy);
     }
 
     public void delete(Long id) throws SQLException {
+        StatementStrategy statementStrategy = new DeleteStatementStrategy(id);
+        jdbcContextForUpdate(statementStrategy);
+    }
+
+    private void jdbcContextForUpdate(StatementStrategy statementStrategy) throws SQLException {
         Connection connection = null;
         PreparedStatement preparedStatement = null;
-
         try {
             connection = dataSource.getConnection();
-
-            preparedStatement = connection.prepareStatement(
-                    "delete from userinfo where id = ?");
-            preparedStatement.setLong(1, id);
-
+            preparedStatement = statementStrategy.makeStatement(connection);
+            //            preparedStatement = connection.prepareStatement(
+//                    "update userinfo set name = ?, password = ? where id = ?");
+//            preparedStatement.setString(1, user.getName());
+//            preparedStatement.setString(2, user.getPassword());
+//            preparedStatement.setLong(3, user.getId());
             preparedStatement.executeUpdate();
         } finally {
             try {
