92ee88002f30fa9df74d7b1dbdb74b7ada375e31
LulzCo <ysw991106@gmail.com>
2023-04-21 05:47:18 +0900

    10. jdbcContext클래스 분리

---
+++ practice/userdao2/src/main/java/kr/ac/jejunu/user/JdbcContext.java
@@ -0,0 +1,126 @@
+package kr.ac.jejunu.user;
+
+import javax.sql.DataSource;
+import java.sql.Connection;
+import java.sql.PreparedStatement;
+import java.sql.ResultSet;
+import java.sql.SQLException;
+
+public class JdbcContext {
+    private final DataSource dataSource;
+
+    public JdbcContext(DataSource dataSource) {
+        this.dataSource = dataSource;
+    }
+
+    User jdbcContextForFind(Long id, StatementStrategy statementStrategy) throws SQLException {
+        Object[] param = {id};
+        PreparedStatement preparedStatement = null;
+        Connection connection = null;
+        ResultSet resultSet = null;
+        User user = null;
+        try {
+            connection = dataSource.getConnection();
+            preparedStatement = statementStrategy.makeStatement(connection, param);
+            resultSet = preparedStatement.executeQuery();
+            if (resultSet.next()) {
+                user = new User();
+                user.setId(resultSet.getLong("id"));
+                user.setName(resultSet.getString("name"));
+                user.setPassword(resultSet.getString("password"));
+            }
+        } finally {
+            try {
+                resultSet.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            try {
+                preparedStatement.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+        return user;
+    }
+
+    void jdbcContextForInsert(User user, StatementStrategy statementStrategy) throws SQLException {
+        Object[] param = {user.getName(), user.getPassword()};
+        PreparedStatement preparedStatement = null;
+        Connection connection = null;
+        ResultSet resultSet = null;
+        try {
+            connection = dataSource.getConnection();
+            preparedStatement = statementStrategy.makeStatement(connection, param);
+            preparedStatement.executeUpdate();
+            resultSet = preparedStatement.getGeneratedKeys();
+            resultSet.next();
+            user.setId(resultSet.getLong(1));
+        } finally {
+            try {
+                resultSet.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            try {
+                preparedStatement.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    void jdbcContextForUpdate(User user, StatementStrategy statementStrategy) throws SQLException {
+        Object[] param = {user.getName(), user.getPassword(), user.getId()};
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        try {
+            connection = dataSource.getConnection();
+            preparedStatement = statementStrategy.makeStatement(connection, param);
+            preparedStatement.executeUpdate();
+        } finally {
+            try {
+                preparedStatement.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+
+    void jdbcContextForDelete(Long id, StatementStrategy statementStrategy) throws SQLException {
+        Object[] param = {id};
+        Connection connection = null;
+        PreparedStatement preparedStatement = null;
+        try {
+            connection = dataSource.getConnection();
+            preparedStatement = statementStrategy.makeStatement(connection, param);
+            preparedStatement.executeUpdate();
+        } finally {
+            try {
+                preparedStatement.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+            try {
+                connection.close();
+            } catch (SQLException e) {
+                e.printStackTrace();
+            }
+        }
+    }
+}
--- practice/userdao2/src/main/java/kr/ac/jejunu/user/UserDao.java
+++ practice/userdao2/src/main/java/kr/ac/jejunu/user/UserDao.java
@@ -1,128 +1,48 @@
 package kr.ac.jejunu.user;
 
-import javax.sql.DataSource;
 import java.sql.*;
 
 public class UserDao {
-    private final DataSource dataSource;
+    private final JdbcContext jdbcContext;
 
-    public UserDao(DataSource dataSource) {
-        this.dataSource = dataSource;
+    public UserDao(JdbcContext jdbcContext) {
+        this.jdbcContext = jdbcContext;
     }
-
 
     public User findById(Long id) throws SQLException {
         StatementStrategy statementStrategy = new FindStatementStrategy();
-        Object[] param = {id};
-        PreparedStatement preparedStatement = null;
-        Connection connection = null;
-        ResultSet resultSet = null;
-        User user = null;
-        try {
-            connection = dataSource.getConnection();
-            preparedStatement = statementStrategy.makeStatement(connection, param);
-            resultSet = preparedStatement.executeQuery();
-            if (resultSet.next()) {
-                user = new User();
-                user.setId(resultSet.getLong("id"));
-                user.setName(resultSet.getString("name"));
-                user.setPassword(resultSet.getString("password"));
-            }
-        } finally {
-            try {
-                resultSet.close();
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-            try {
-                preparedStatement.close();
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-            try {
-                connection.close();
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-        }
-        return user;
+        return jdbcContext.jdbcContextForFind(id, statementStrategy);
+    }
+
+    private User jdbcContextForFind(Long id, StatementStrategy statementStrategy) throws SQLException {
+        return jdbcContext.jdbcContextForFind(id, statementStrategy);
     }
 
     public void insert(User user) throws SQLException {
         StatementStrategy statementStrategy = new InsertStatementStrategy();
-        Object[] param = {user.getName(), user.getPassword()};
-        PreparedStatement preparedStatement = null;
-        Connection connection = null;
-        ResultSet resultSet = null;
-        try {
-            connection = dataSource.getConnection();
-            preparedStatement = statementStrategy.makeStatement(connection, param);
-            preparedStatement.executeUpdate();
-            resultSet = preparedStatement.getGeneratedKeys();
-            resultSet.next();
-            user.setId(resultSet.getLong(1));
-        } finally {
-            try {
-                resultSet.close();
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-            try {
-                preparedStatement.close();
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-            try {
-                connection.close();
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-        }
+        jdbcContext.jdbcContextForInsert(user, statementStrategy);
+    }
+
+    private void jdbcContextForInsert(User user, StatementStrategy statementStrategy) throws SQLException {
+        jdbcContext.jdbcContextForInsert(user, statementStrategy);
     }
 
     public void update(User user) throws SQLException {
         StatementStrategy statementStrategy = new UpdateStatementStrategy();
-        Object[] param = {user.getName(), user.getPassword(), user.getId()};
-        Connection connection = null;
-        PreparedStatement preparedStatement = null;
-        try {
-            connection = dataSource.getConnection();
-            preparedStatement = statementStrategy.makeStatement(connection, param);
-            preparedStatement.executeUpdate();
-        } finally {
-            try {
-                preparedStatement.close();
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-            try {
-                connection.close();
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-        }
+        jdbcContext.jdbcContextForUpdate(user, statementStrategy);
+    }
+
+    private void jdbcContextForUpdate(User user, StatementStrategy statementStrategy) throws SQLException {
+        jdbcContext.jdbcContextForUpdate(user, statementStrategy);
     }
 
     public void delete(Long id) throws SQLException {
         StatementStrategy statementStrategy = new DeleteStatementStrategy();
-        Object[] param = {id};
-        Connection connection = null;
-        PreparedStatement preparedStatement = null;
-        try {
-            connection = dataSource.getConnection();
-            preparedStatement = statementStrategy.makeStatement(connection, param);
-            preparedStatement.executeUpdate();
-        } finally {
-            try {
-                preparedStatement.close();
-            } catch (SQLException e) {
-                e.printStackTrace();
-            } try {
-                connection.close();
-            } catch (SQLException e) {
-                e.printStackTrace();
-            }
-        }
+        jdbcContext.jdbcContextForDelete(id, statementStrategy);
+    }
+
+    private void jdbcContextForDelete(Long id, StatementStrategy statementStrategy) throws SQLException {
+        jdbcContext.jdbcContextForDelete(id, statementStrategy);
     }
 
 }
