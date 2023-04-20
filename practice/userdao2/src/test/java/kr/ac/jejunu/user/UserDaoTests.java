0ca6e2255b48e84e1dcc862eab5f235fbcd804a1
LulzCo <ysw991106@gmail.com>
2023-04-21 01:57:06 +0900

    2. jeju,halla 분리

--- practice/userdao2/src/test/java/kr/ac/jejunu/user/UserDaoTests.java
+++ practice/userdao2/src/test/java/kr/ac/jejunu/user/UserDaoTests.java
@@ -10,11 +10,11 @@ import static org.hamcrest.core.Is.is;
 
 public class UserDaoTests {
     @Test
-    public void get() throws SQLException, ClassNotFoundException {
+    public void getForJeju() throws SQLException, ClassNotFoundException {
         Long id = 1l;
         String name = "hulk";
         String password = "1234";
-        UserDao userDao = new UserDao();
+        UserDao userDao = new JejuUserDao();
         User user = userDao.findById(id);
         assertThat(user.getId(), is(id));
         assertThat(user.getName(), is(name));
@@ -22,14 +22,14 @@ public class UserDaoTests {
     }
 
     @Test
-    public void insert() throws SQLException, ClassNotFoundException {
+    public void insertForJeju() throws SQLException, ClassNotFoundException {
         String name = "hulk";
         String password = "1234";
         User user = new User();
         user.setName(name);
         user.setPassword(password);
 
-        UserDao userDao = new UserDao();
+        UserDao userDao = new JejuUserDao();
         userDao.insert(user);
 
         assertThat(user.getId(), greaterThan(1L));
@@ -38,6 +38,36 @@ public class UserDaoTests {
 
         assertThat(insertedUser.getName(), is(user.getName()));
         assertThat(insertedUser.getPassword(), is(user.getPassword()));
+    }
 
+    @Test
+    public void getForHalla() throws SQLException, ClassNotFoundException {
+        Long id = 1l;
+        String name = "hulk";
+        String password = "1234";
+        UserDao userDao = new HallaUserDao();
+        User user = userDao.findById(id);
+        assertThat(user.getId(), is(id));
+        assertThat(user.getName(), is(name));
+        assertThat(user.getPassword(), is(password));
+    }
+
+    @Test
+    public void insertForHalla() throws SQLException, ClassNotFoundException {
+        String name = "hulk";
+        String password = "1234";
+        User user = new User();
+        user.setName(name);
+        user.setPassword(password);
+
+        UserDao userDao = new HallaUserDao();
+        userDao.insert(user);
+
+        assertThat(user.getId(), greaterThan(1L));
+
+        User insertedUser = userDao.findById(user.getId());
+
+        assertThat(insertedUser.getName(), is(user.getName()));
+        assertThat(insertedUser.getPassword(), is(user.getPassword()));
     }
 }
